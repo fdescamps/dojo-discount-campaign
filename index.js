7d3a14ff1598bff39e144e1f9c4f191bc728a018
unknown <S636246@PO446682.siege.axa-fr.intraxa>
2016-11-30 18:27:32 +0100

    feat(ramda): add dependency

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var _ = require('lodash');
-var moment = require('moment');
+const R = require('ramda');
+const moment = require('moment');
 
 /*
  * Data for the dojo
  */
-var customers = require('./customer.json');
+const customers = require('./customer.json');
 
 /*
 The conditions for participation for customers are defined as follows:
@@ -17,26 +17,121 @@ The conditions for participation for customers are defined as follows:
     b) the total revenue throughout the entire year 2015
 */
 
-var start = moment();
+const start = moment();
 
-var WANTED_STATUS = ['student','pupil','apprentice'];
-var LIMIT_AGE = 25;
-var YEAR = 2015;
-
+const WANTED_STATUS = ['student','pupil','apprentice'];
+const LIMIT_AGE = 25;
+const YEAR = 2015;
+/*
 var orders = _(customers)
-    .filter(customer => customer.age <= LIMIT_AGE && _.includes(WANTED_STATUS, customer.job))
+    .filter(customer => customer.age <= LIMIT_AGE && _(WANTED_STATUS).contains(customer.job))
     .map(customer => customer.orders)
     .flatten()
     .filter(order => moment(order.date).year() === YEAR)
     .value();
-
+     
 var result = _(orders)
     .groupBy(order => moment(order.date).month())
     .mapValues(orderByMonth => _.sumBy(orderByMonth,'total'))
-    .value();
+    .value();     
+        
+var result = _(orders)
+    .groupBy( order => moment(order.date).month())
+    .map( month => _(month).sum('total') )
+    .value();*/
+
+const isAnElligibleCustomer = customer => customer.age <= LIMIT_AGE && R.contains( customer.job, WANTED_STATUS );
+let result = R.filter( isAnElligibleCustomer, customers );
+result = R.map( customer => customer.orders , result );
+result = R.flatten( result );
+result = R.filter( order => moment(order.date).year() === YEAR, result );
+
+/*
+var numbers = [1, 2, 3];
+var plus = (a, b) => a + b;
+R.reduce(plus, 10, numbers); //=> 16
+*/
+
+//let ordersByMonth = R.groupBy( order => moment(order.date).month() );
+//let byMonth = R.reduce( , 0.0, ordersByMonth); R.map( month => _(month).sum('total'), byGrade );
+//let byMonth = R.compose( R.key, R.map )( ordersByMonth )
+
+let addMonth = R.map( order => { order.month = moment(order.date).month(); return order; }, result );
+let sortByMonth = R.sortBy( R.prop( 'month' ) );
+//console.log( 'sortByMonth: ' + JSON.stringify( sortByMonth( addMonth, null, 4 ) ) );
+let ordersByMonth = R.groupWith( R.eqProps( 'month' ), sortByMonth( addMonth ) );
+console.log( 'ordersByMonth: ' + JSON.stringify( ordersByMonth, null, 4 ) );
+
+
+
+
+let plus = (o1, o2) => { 
+        console.log('====> '+JSON.stringify(o1)+','+JSON.stringify(o1)); 
+        return o1.total+o2.total; 
+    };
+let sumByMonth = R.map( 
+    (orders) => { 
+        console.log('======> '+JSON.stringify(orders,null,4)); 
+        return R.reduce( plus, 0.0, orders );
+    }, 
+    ordersByMonth);
+    
+console.log( 'sumByMonth: ' + JSON.stringify( sumByMonth, null, 4 ) );
+
+
 
 /**
  * Display result
  */
-console.log( JSON.stringify( result, null, 4 ) );
-console.log('Total: '+ _.sum(_.valuesIn(result)) + ' (compute duration: '+ (moment()-start) +'ms)');
+console.log( 'NB customers: ' + result.length );
+console.log( 'Total: ' + JSON.stringify( sumByMonth, null, 4 ) );
+//console.log('Total: '+ R.sum(R.valuesIn(result)) + ' (compute duration: '+ (moment()-start) +'ms)');
+//console.log( 'Total: ' + _(result).sum() + ' (compute duration: '+ (moment()-start) +'ms) ')
+
+/*
+var numbers = [1, 2, 3];
+var plus = (a, b) => a + b;
+
+R.reduce(plus, 10, numbers); //=> 16
+
+
+
+R.groupWith(R.equals, [0, 1, 1, 2, 3, 5, 8, 13, 21])
+//=> [[0], [1, 1], [2], [3], [5], [8], [13], [21]]
+
+R.groupWith((a, b) => a % 2 === b % 2, [0, 1, 1, 2, 3, 5, 8, 13, 21])
+//=> [[0], [1, 1], [2], [3, 5], [8], [13, 21]]
+
+R.groupWith(R.eqBy(isVowel), 'aestiou')
+//=> ['ae', 'st', 'iou']
+
+
+
+var f = R.compose(R.inc, R.negate, Math.pow);
+
+f(3, 4); // -(3^4) + 1
+
+
+
+
+var reduceToNamesBy = R.reduceBy((acc, student) => acc.concat(student.name), []);
+var namesByGrade = reduceToNamesBy(function(student) {
+  var score = student.score;
+  return score < 65 ? 'F' :
+         score < 70 ? 'D' :
+         score < 80 ? 'C' :
+         score < 90 ? 'B' : 'A';
+});
+var students = [{name: 'Lucy', score: 92},
+                {name: 'Drew', score: 85},
+                // ...
+                {name: 'Bart', score: 62}];
+namesByGrade(students);
+// {
+//   'A': ['Lucy'],
+//   'B': ['Drew']
+//   // ...,
+//   'F': ['Bart']
+// }
+
+*/
