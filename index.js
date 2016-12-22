c0039cfb723c8c93d5d95541fc86bda0ced3590a
François Descamps <francois.descamps@axa.fr>
2016-12-22 09:21:08 +0100

    feat(transducers): the same algo but with two transducers

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var _ = require('lodash');
-var moment = require('moment');
+const R = require('ramda');
+const moment = require('moment');
 
 /*
  * Data for the dojo
  */
-var customers = require('./customer.json');
+const data = require('./customer.json');
 
 /*
 The conditions for participation for customers are defined as follows:
@@ -17,16 +17,44 @@ The conditions for participation for customers are defined as follows:
     b) the total revenue throughout the entire year 2015
 */
 
-var start = moment();
+const start = moment();
 
-var WANTED_STATUS = ['student','pupil','apprentice'];
-var LIMIT_AGE = 25;
-var YEAR = 2015;
+const WANTED_STATUS = ['student','pupil','apprentice'];
+const LIMIT_AGE = 25;
+const YEAR = 2015;
 
-var result = {};
+/** Utils Method */
+const reduce = (xs, x) => { xs.push(x); return xs; }
+const flatten = (f) => (reducing) => (result, input) => f(result,input);
+const mapping = (f) => (reducing) => (result, input) => reducing( result, f(input) );
+const filtering = (predicate) => (reducing) => (result, input) => predicate(input) ? reducing(result, input) : result;
+
+/** Algo Part 1 */
+const transduceCustomersInOrders = R.compose(
+    filtering( customer => customer.age <= LIMIT_AGE && R.contains( customer.job, WANTED_STATUS ) ),
+    mapping( customer => customer.orders ),
+    flatten( (orders,customerOrders) => R.concat(orders, customerOrders) )
+);
+let customers = data.reduce( transduceCustomersInOrders( reduce ), []);
+
+/** Algo Part 2 */
+const transduceOrdersInEuros = R.compose(
+    filtering( order => { return moment(order.date).year() === YEAR;} ),
+    mapping( order => { order.month = moment(order.date).month(); return order; } )
+);
+let orders = customers.reduce( transduceOrdersInEuros( reduce ), []);
+
+/** Algo Part 3 */
+let computeRevenus = R.pipe(
+    R.sortBy( R.prop( 'month' ) ),
+    R.groupWith( R.eqProps( 'month' ) ),
+    R.map( (orders) =>  R.reduce( (acc, order) => acc+order.total, 0, orders ) )
+);
+let sumByMonth = computeRevenus( orders );
 
 /**
  * Display result
  */
-console.log( JSON.stringify( result.totalByMonth, null, 4 ) );
-console.log('Total: '+ result.totalAmount + ' (compute duration: '+ (moment()-start) +'ms)');
+console.log( 'NB orders to analyze:',orders.length );
+console.log( 'Total by month:', JSON.stringify( sumByMonth, null, 4 ) );
+console.log( '(compute duration:', (moment()-start) +'ms)');
